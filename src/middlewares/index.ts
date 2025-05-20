935c81ab7cc4302ccdf7606b077012f8a421748d
Syomei Aoshima <neko@asyomei.org>
2025-05-20 23:22:30 +0500

    build: use local auto-thread

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,9 @@
 import type { Bot } from 'grammy'
 import type { MyContext } from '~/types/context'
 import { autoRetry } from '@grammyjs/auto-retry'
-import { autoThread } from '@grammyjs/auto-thread'
 import { sequentialize } from '@grammyjs/runner'
 import { apiThrottler } from '@grammyjs/transformer-throttler'
+import { autoThread } from './auto-thread'
 
 export function setupMiddlewares(bot: Bot<MyContext>): void {
   bot.api.config.use(autoRetry(), apiThrottler())
