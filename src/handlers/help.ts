a45053ed5d4fd1726c6f5b40e987a8b43283ed3d
Syomei Aoshima <neko@asyomei.org>
2025-05-20 23:07:41 +0500

    feat: add help handler

---
+++ src/handlers/help.ts
@@ -0,0 +1,28 @@
+import dedent from 'dedent'
+import { Composer } from 'grammy'
+import { escapeHTML } from '~/utils/escape-html'
+
+export const HELP = dedent`
+  <b>Himawari</b> - бот для поиска аниме/манги/персонажей из Шикимори
+
+  <b>Использование:</b>
+  /anime [запрос] - Поиск аниме
+  /manga [запрос] - Поиск манги
+  /character [запрос] - Поиск персонажа
+  <i>Инлайн режим</i> (@%s ${escapeHTML('<команда>')}):
+  anime ИЛИ аниме [запрос] - Поиск аниме
+  manga ИЛИ манга [запрос] - Поиск манга
+  character ИЛИ персонаж [запрос] - Поиск персонажа
+
+  <a href="https://framagit.org/asyomei/himawari-bot">Исходный код</a>
+`
+
+export const helpHandler = new Composer()
+
+helpHandler.on('message:text').command(['start', 'help'], async (ctx) => {
+  const text = HELP.replace('%s', ctx.me.username)
+  await ctx.reply(text, {
+    parse_mode: 'HTML',
+    link_preview_options: { is_disabled: true },
+  })
+})
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,12 @@
 import type { Bot } from 'grammy'
 import type { MyContext } from '~/types/context'
+import { helpHandler } from './help'
 import { nopHandler } from './nop'
 import { shikimoriHandler } from './shikimori'
 
 export function setupHandlers(bot: Bot<MyContext>): void {
   bot.use(
+    helpHandler,
     shikimoriHandler,
     nopHandler,
   )
