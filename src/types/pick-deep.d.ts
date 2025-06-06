9ce6f0464583a7f81f5b2930dff02965afc2b625
Syomei Aoshima <neko@asyomei.org>
2025-06-07 02:41:53 +0500

    lint

--- src/types/pick-deep.d.ts
+++ src/types/pick-deep.d.ts
@@ -2,9 +2,9 @@ type Head<T extends string> = T extends `${infer First}.${string}` ? First : T
 
 type Tail<T extends string> = T extends `${string}.${infer Rest}` ? Rest : never
 
-type PartialOnUndefined<T> =
-  & { [K in keyof T as undefined extends T[K] ? never : K]: T[K] }
-  & { [K in keyof T as undefined extends T[K] ? K : never]?: T[K] }
+type PartialOnUndefined<T>
+  = & { [K in keyof T as undefined extends T[K] ? never : K]: T[K] }
+    & { [K in keyof T as undefined extends T[K] ? K : never]?: T[K] }
 
 export type PickDeep<T, K extends string> = PartialOnUndefined<
   T extends object
