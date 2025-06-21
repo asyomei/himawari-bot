46461dd0e83c9fc5e3f202210aaa8ae74252eaf8
Syomei Aoshima <neko@asyomei.org>
2025-06-21 16:54:43 +0500

    fix(escape-html): dont escape apos

--- src/utils/escape-html.ts
+++ src/utils/escape-html.ts
@@ -1,11 +1,10 @@
 const lookup = {
   '&': '&amp;',
   '"': '&quot;',
-  '\'': '&apos;',
   '<': '&lt;',
   '>': '&gt;',
 }
 
 export function escapeHTML(s: string): string {
-  return s.replace(/[&"'<>]/g, c => lookup[c as never])
+  return s.replace(/[&"<>]/g, c => lookup[c as never])
 }
